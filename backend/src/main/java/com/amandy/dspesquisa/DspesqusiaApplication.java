0f5842d5f9cf266b74b3b8678bb39c68e1b4773f
marcioamandy <marcioamandy@gmail.com>
2020-09-19 19:16:56 -0300

    Project Created

---
+++ backend/src/main/java/com/amandy/dspesquisa/DspesqusiaApplication.java
@@ -0,0 +1,13 @@
+package com.amandy.dspesquisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DspesqusiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DspesqusiaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/amandy/dspesquisa/DspesqusiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amandy.dspesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DspesqusiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

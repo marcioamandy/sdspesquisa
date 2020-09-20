b6d4a1cfa43a2bb5e619cf89ab8afc6ecc231b1a
marcioamandy <marcioamandy@gmail.com>
2020-09-20 01:25:04 -0300

    GET Records

--- backend/src/main/java/com/amandy/dspesquisa/repositories/RecordRepository.java
+++ backend/src/main/java/com/amandy/dspesquisa/repositories/RecordRepository.java
@@ -1,11 +1,21 @@
 package com.amandy.dspesquisa.repositories;
 
+import java.time.Instant;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.amandy.dspesquisa.entities.Record;
 
 @Repository
 public interface RecordRepository extends JpaRepository<Record, Long>{
+
+	@Query("SELECT obj FROM Record obj WHERE "
+			+ "(coalesce(:min, null) IS NULL OR obj.moment >= :min) AND "
+			+ "(coalesce(:max, null) IS NULL OR obj.moment <= :max)")
+	Page<Record> findBymoments(Instant min, Instant max, Pageable pageable);
 
 }
